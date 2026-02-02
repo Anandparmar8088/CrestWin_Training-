fc0b244b24127a67cd1e10b7682e7c0c3c4c1785
Anandparmar8088 <parmaranand031@gmail.com>
2026-02-02 18:25:39 +0530

    py basics

---
+++ Practice/Basics.py
@@ -0,0 +1,76 @@
+#basics of python for Day one training tasks
+
+
+
+
+ # DATA TYPES IN PYTHON   #
+ 
+ 
+ 
+
+# Numeric  int 
+x = 10
+
+# float or Decimal numbers
+
+y = 10.5
+
+# complex Real num + the  imaginary
+
+z = 3 + 4j
+
+# Boolean bool
+
+True or False
+
+a = True
+b = False
+
+
+
+
+
+
+#  Sequence Types  string
+
+# immutable sequence of characters
+
+name = "Anand"
+
+# list :- Ordered  mutable, allows duplicate
+
+nums = [1, 2, 3]
+
+# tuple data type :- Ordered, immutable, allows duplicates
+
+t = (1, 2, 3)
+
+
+
+
+# range : -Sequence of numbers
+
+r = range(1, 5)
+
+#  Set Types  Unordered, mutable, no duplicates
+
+s = {1, 2, 3}
+
+
+
+#  mapping the values in key value pair Dict is used : Key-value pairs
+
+student = {"name": "Anand", "age": 22}
+
+
+
+
+
+# none Type None
+
+#  for presenting absance of any num we use the none
+
+x = None
+
+#  for checking the Type of data type is  used 
+type(x)
